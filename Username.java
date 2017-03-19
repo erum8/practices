c4f3b0ded3b54ec54c0267fd90a86222b2905700
erum8 <erum8@users.noreply.github.com>
2017-03-19 04:08:37 -0700

    Create Username.java

---
+++ Username.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class UserName
+{
+public static void main(string[]args)
+{
+string firstname;
+string lastname;
+Scanner input=new.Scanner(system.in);
+firstname=Scane.nextLine();
+lastname=Scane.nextLine();
+system.out.print("name is:"+firstname+lastname);
+}
+}
