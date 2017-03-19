015514f56b093112aa10000f86be9cc746bad58f
erum8 <erum8@users.noreply.github.com>
2017-03-19 03:59:53 -0700

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,21 @@
+import java.util.Scanner
+public class Input
+{
+public static void main(string[]args)
+{
+Scanner Scan=new Scanner(system.in)
+float hight;
+system.out.print("enter your hight");
+hight=Scan.nextFloat();
+system.out.print("your height is"+hight);
+}
+}
+
+
+
+
+
+
+
+
+'
