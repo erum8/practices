d9276b8bdfcade55690e1e27c80fdd7c7c5919db
erum8 <erum8@users.noreply.github.com>
2017-03-19 04:17:29 -0700

    Create cube.java

---
+++ cube.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.math;
+public class cube
+{
+public static void main(string[]args)
+{
+int num1=0;
+int num2=0;
+double cube=0;
+Scannner scan=new scanner(system.in);
+num1=scan.nextInt();
+num2=Scan.nextInt();
+cube =math.pow(num1,3)+math.pow(num2,3);
+system.out.print("cube":+cube);
+}
+}
